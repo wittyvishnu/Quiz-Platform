66ca1523b9cef85b8a830d3ac9472d40e028d8ea
wittyvishnu <vishnuwitty@gmail.com>
2025-06-06 22:26:54 +0530

    completed project

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    memoryBasedWorkers: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  productionBrowserSourceMaps: false,
+};
 
 export default nextConfig;
